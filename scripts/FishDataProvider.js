3490743545a684c2596f361779993ac5cdb69ca2
peytonkemp <peyton.kemp@gmail.com>
2021-01-13 12:04:37 -0600

    added images in the data provider. Also added specifications to the array

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,23 +1,44 @@
 const fishCollection = [
     {
-        name: "Bart",
-        food: "crustaceans"
+        img: "https://i.dlpng.com/static/png/6951024_preview.png",
+        name: "Nemo",
+        species:"",
+        length:"",
+        location:"",
+        diet: "crustaceans"
+        
     },
     {
-        name: "David",
-        food: "crustaceans"
+        img: "https://www.aquariumofpacific.org/images/made_new/images-uploads-clownfish_600_q85.jpg",
+        name: "Marlin",
+        species:"",
+        length:"",
+        location:"",
+        diet: "crustaceans"
     },
     {
+        img: "https://images.unsplash.com/photo-1524704654690-b56c05c78a00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
         name: "Frank",
-        food: "crustaceans"
+        species:"",
+        length:"",
+        location:"",
+        diet: "crustaceans"
     },
     {
-        name: "Bill",
-        food: "crustaceans"
+        img: "https://images.immediate.co.uk/production/volatile/sites/4/2009/07/GettyImages-931270318-43ab672.jpg?quality=90&resize=940%2C400",
+        name: "Billy",
+        species:"Goldfish",
+        length:"",
+        location:"", 
+        diet: "crustaceans"
     },
     {
+        img: "https://images.unsplash.com/photo-1522720833375-9c27ffb02a5e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXF1YXJpdW0lMjBmaXNofGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80",
         name: "Sammy",
-        food: "crustaceans"
+        species:"",
+        length:"",
+        location:"",
+        diet: "crustaceans"
     },
 ]
 
